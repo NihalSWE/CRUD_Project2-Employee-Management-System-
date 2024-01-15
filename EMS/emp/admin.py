36069ef743e7b38dfcbaea61cf57dd61660086be
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-01-16 03:32:31 +0600

    slove some problems , create model & save some data in database

--- EMS/emp/admin.py
+++ EMS/emp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Department,Role,Employee
 
 # Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display=('name','location')
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display=['name']
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin): # A comma-separated
+    list_display = ('id','first_name', 'last_name','email', 'phone', 'dept', 'role', 'salary')
+    search_fields = ['first_name', 'last_name'] 
+
+
+    def dept(self, obj):
+        return obj.dept.name
+
+    def role(self, obj):
+        return obj.role.name
--- EMS/emp/models.py
+++ EMS/emp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50,null=False)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=12, blank=True)
+    dept = models.ForeignKey(Department,on_delete=models.CASCADE)
+    role = models.ForeignKey(Role,on_delete=models.CASCADE)
+    salary = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.first_name
