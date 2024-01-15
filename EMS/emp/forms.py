af513d80c7701245da37f52919af72b88c50ee2d
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-01-16 04:35:39 +0600

    Create model base form

---
+++ EMS/emp/forms.py
@@ -0,0 +1,8 @@
+from .models import Department,Role,Employee
+from django import forms
+
+
+class EmployeeRegistration(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields=['first_name','last_name', 'email', 'phone','dept','role','salary']
--- EMS/emp/views.py
+++ EMS/emp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Employee,Role,Department
+from .forms import EmployeeRegistration
 
 # Create your views here.
 
@@ -6,10 +8,18 @@ def base(request):
     return render(request, 'base.html')
 
 def allemp(request):
-    return render(request,'all_emp.html')
+    emps = Employee.objects.all()
+    context ={
+        'emps':emps
+    }
+    return render(request,'all_emp.html',context)
 
 def add(request):
-    return render(request,'add_emp.html')
+    if request.method == 'POST':
+        fm = EmployeeRegistration(request.POST)
+    else:
+        fm = EmployeeRegistration()
+    return render(request,'add_emp.html',{'form':fm})
 
 def remove(request):
     return render(request,'remove.html')
